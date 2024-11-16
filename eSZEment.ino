0f8e5286b0c877e1e1f583a3d628b655765a354e
4f48 <0x4f48@proton.me>
2024-11-16 14:22:44 +0100

    teapot vizualizacio, valodi gyorsulas

--- eSZEment.ino
+++ eSZEment.ino
@@ -1,24 +1,28 @@
 #include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps612.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
 MPU6050 mpu;
 
-int const INTERRUPT_PIN = 2;  // Define the interruption #0 pin
+int const INTERRUPT_PIN = 2;
 bool blinkState;
 
+/*---MPU6050 Control/Status Variables---*/
 bool DMPReady = false;  // Set true if DMP init was successful
 uint8_t MPUIntStatus;   // Holds actual interrupt status byte from MPU
 uint8_t devStatus;      // Return status after each device operation (0 = success, !0 = error)
 uint16_t packetSize;    // Expected DMP packet size (default is 42 bytes)
 uint8_t FIFOBuffer[64]; // FIFO storage buffer
 
+/*---Orientation/Motion Variables---*/ 
 Quaternion q;           // [w, x, y, z]         Quaternion container
 VectorInt16 aa;         // [x, y, z]            Accel sensor measurements
 VectorInt16 gy;         // [x, y, z]            Gyro sensor measurements
 VectorInt16 aaReal;     // [x, y, z]            Gravity-free accel sensor measurements
 VectorInt16 aaWorld;    // [x, y, z]            World-frame accel sensor measurements
 VectorFloat gravity;    // [x, y, z]            Gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   Yaw/Pitch/Roll container and gravity vector
+
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
 
 volatile bool MPUInterrupt = false;
 void DMPDataReady() {
@@ -28,12 +32,12 @@ void DMPDataReady() {
 void setup() {
   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     Wire.begin();
-    Wire.setClock(400000);
+    Wire.setClock(400000); 
   #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
     Fastwire::setup(400, true);
   #endif
   
-  Serial.begin(115200);
+  Serial.begin(115200); 
   while (!Serial);
 
   Serial.println(F("Initializing I2C devices..."));
@@ -49,15 +53,15 @@ void setup() {
     Serial.println("MPU6050 connection successful");
   }
 
-  Serial.println(F("\nSend any character to begin: "));
-  while (Serial.available() && Serial.read());
-  while (!Serial.available());
-  while (Serial.available() && Serial.read());
-
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
 
-  mpu.setZAccelOffset(-1000);
+  mpu.setXGyroOffset(0);
+  mpu.setYGyroOffset(0);
+  mpu.setZGyroOffset(0);
+  mpu.setXAccelOffset(0);
+  mpu.setYAccelOffset(0);
+  mpu.setZAccelOffset(0);
 
   if (devStatus == 0) {
     mpu.CalibrateAccel(6);
@@ -86,18 +90,37 @@ void setup() {
 
 void loop() {
   if (!DMPReady) return;
-    
   if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) {
+      mpu.dmpGetQuaternion(&q, FIFOBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      Serial.print("ypr\t");
+      Serial.print(ypr[0] * 180/M_PI);
+      Serial.print("\t");
+      Serial.print(ypr[1] * 180/M_PI);
+      Serial.print("\t");
+      Serial.println(ypr[2] * 180/M_PI);
+
       mpu.dmpGetQuaternion(&q, FIFOBuffer);
       mpu.dmpGetAccel(&aa, FIFOBuffer);
       mpu.dmpGetGravity(&gravity, &q);
       mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-      mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-      Serial.print("X,Y,Z:\t");
-      Serial.print(aaWorld.x);
-      Serial.print(",\t");
-      Serial.print(aaWorld.y);
-      Serial.print(",\t");
-      Serial.println(aaWorld.z);
+      Serial.print("areal\t");
+      Serial.print(aaReal.x);
+      Serial.print("\t");
+      Serial.print(aaReal.y);
+      Serial.print("\t");
+      Serial.println(aaReal.z);
+
+      teapotPacket[2] = FIFOBuffer[0];
+      teapotPacket[3] = FIFOBuffer[1];
+      teapotPacket[4] = FIFOBuffer[4];
+      teapotPacket[5] = FIFOBuffer[5];
+      teapotPacket[6] = FIFOBuffer[8];
+      teapotPacket[7] = FIFOBuffer[9];
+      teapotPacket[8] = FIFOBuffer[12];
+      teapotPacket[9] = FIFOBuffer[13];
+      Serial.write(teapotPacket, 14);
+      teapotPacket[11]++;
   }
 }
